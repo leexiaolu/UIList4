e60f707977ce31b8a2d76b9708e7388148719081
leexiaolu <573702124@qq.com>
2018-03-04 17:54:47 +0800

    viewpager 1st commit

--- app/src/main/java/com/example/leesnriud/uilist4/ViewPagerActivity.java
+++ app/src/main/java/com/example/leesnriud/uilist4/ViewPagerActivity.java
@@ -1,13 +1,38 @@
 package com.example.leesnriud.uilist4;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+
+import java.util.ArrayList;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class ViewPagerActivity extends AppCompatActivity {
+
+
+    private ArrayList<View> aList;
+    private MyPagerAdapter mAdapter;
+
+    @BindView(R.id.vp_viewpager_one)
+    ViewPager vpViewpagerOne;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_pager);
+        ButterKnife.bind(this);
+
+        aList = new ArrayList<View>();
+        LayoutInflater layoutInflater = getLayoutInflater();
+        aList.add(layoutInflater.inflate(R.layout.layout_pager_one,null,false));
+        aList.add(layoutInflater.inflate(R.layout.layout_pager_two,null,false));
+        aList.add(layoutInflater.inflate(R.layout.layout_pager_three,null,false));
+
+        mAdapter = new MyPagerAdapter(aList);
+        vpViewpagerOne.setAdapter(mAdapter);
     }
 }
